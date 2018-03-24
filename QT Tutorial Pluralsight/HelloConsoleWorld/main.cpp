de1959574431929118dc9fe5e8e16e8079c6512b
Michael Hendricks <MichaelH@dipar.local>
2018-03-24 08:47:39 +0200

    QString,QWarning,QTextStream

--- QT Tutorial Pluralsight/HelloConsoleWorld/main.cpp
+++ QT Tutorial Pluralsight/HelloConsoleWorld/main.cpp
@@ -1,9 +1,21 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     //    QCoreApplication a(argc, argv);
+    QString msg ("Hi there this is Mike");
+    qWarning() << "Hello Console"<<7<<"Mike";
+    qWarning().nospace() << "Hello Console"<<7<<"Mike";
+    qWarning() << msg;
+    qWarning(msg.toStdString().c_str());
+    qWarning(qPrintable(msg));
 
+    QTextStream out_stream(stdout);
+    out_stream.setFieldWidth(10);
+    out_stream.setPadChar('*');
+    out_stream << "QTextStream:\n" << msg << 7 << "\n";
+
+    //    qWarning("Hello Console");
     //    return a.exec();
-    qWarning("Hello Console");
 }
